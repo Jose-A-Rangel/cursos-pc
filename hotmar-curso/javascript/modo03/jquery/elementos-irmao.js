ce41bb6871f1807c48a006d8cee329e87618fe44
jose-rangel <jose.estudor@hotmail.com>
2025-10-05 22:04:05 -0300

    exe1

---
+++ hotmar-curso/javascript/modo03/jquery/elementos-irmao.js
@@ -0,0 +1,33 @@
+
+/*
+let selected=$("#div3").siblings("#div4");
+console.log(selected)
+pega tds os irmao, porem nao vai pegar o que ta refenciado.
+*/
+
+/*
+let selected=$("#div3").nextAll();
+console.log(selected)
+pega todos os irmaos que vem depois do elemento referenciado.
+*/
+
+
+/*
+let selected=$("#div3").next();
+console.log(selected)
+vai pegar o proximo irmao do elemento referenciado.
+*/
+
+/*
+let selected=$("#div3").prev();
+console.log(selected)
+pega o irmao anterior do elemento referenciado.
+*/
+
+/*
+let selected=$("#div3").prevAll();
+console.log(selected)
+pega tds os irmaos que vem antes do elemento referenciado.
+*/
+
+
